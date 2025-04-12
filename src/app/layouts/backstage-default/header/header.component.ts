693ac27167e57078af93af132bd49be1f0cfb575
KienNguyen <bakien.nguyen@kohyoung.com>
2025-04-12 10:48:48 +0700

    hiển thị tên của user trên header thay cho tên demo fix

--- src/app/layouts/backstage-default/header/header.component.ts
+++ src/app/layouts/backstage-default/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import {TokenStorageService} from "@core/services/token-storage.service";
 import {NotificationService} from "@core/services/notification.service";
+import {UserInfo} from "@core/models/auth.models";
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,15 @@ export class HeaderComponent implements OnInit {
 
   @Input() isCollapsed: boolean;
 
+  user: UserInfo;
+
   constructor(
     private router: Router,
     private tokenStorage: TokenStorageService,
     private notification: NotificationService,
-  ) { }
+  ) {
+    this.user = this.tokenStorage.getUser();
+  }
 
   ngOnInit() {
   }
