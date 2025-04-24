c78f4544dbbe4e78d8307ed4df5c3246419ffe0c
Bryan <bryan.le@kohyoung.com>
2025-04-24 17:18:52 +0700

    update notice

--- src/app/pages/backstage/admin/data/admin.service.ts
+++ src/app/pages/backstage/admin/data/admin.service.ts
@@ -13,14 +13,6 @@ export class AdminService extends BaseApiService {
 
   private readonly BASE_URL = this.buildUrl('/admin');
 
-
-  /**
-   * Lấy thông tin người dùng
-   */
-  getUserProfile(userId: number): Observable<ApiResponse<UserDTO>> {
-    return this.http.get<ApiResponse<UserDTO>>(`${this.BASE_URL}/users/${userId}`);
-  }
-
   getAllUsers(
   ): Observable<ApiResponse<UserDTO[]>> {
     // Gửi request GET với query string được cấu hình
@@ -38,6 +30,11 @@ export class AdminService extends BaseApiService {
     });
   }
 
+  updateUserStatus(id: number, userDTO: UserDTO) {
+    return this.http.put(`${this.BASE_URL}/users/${id}/status`, { userDTO });
+  }
+
+
   updateBusinessProfile(id: number, userDTO: UserDTO) {
     return this.http.put<ApiResponse<string>>(`${this.BASE_URL}/users/${id}/updateBusinessProfile`, userDTO, {
       headers: new HttpHeaders({
@@ -46,23 +43,8 @@ export class AdminService extends BaseApiService {
     });
   }
 
-  updateUserStatus(id: number, userDTO: UserDTO) {
-    return this.http.put(`${this.BASE_URL}/users/${id}/status`, { userDTO });
-  }
-
   updateBusinessStatus(id: number, userDTO: UserDTO) {
     return this.http.put(`${this.BASE_URL}/users/${id}/business-status`, { userDTO });
-  }
-
-  getUserStatusOptions(): Observable<{ label: string, value: string }[]> {
-    return new Observable(observer => {
-      observer.next([
-        { label: 'ACTIVE', value: 'ACTIVE' },
-        { label: 'ACTIVE', value: 'ACTIVE' },
-        { label: 'PENDING', value: 'PENDING' }
-      ]);
-      observer.complete();
-    });
   }
 
 }
